31fb47d8a9bfe50414c4a4ede57bc48157d5aa1e
Ben Sarak-Jones <bensarakjones@gmail.com>
2023-03-28 22:52:18 +0100

    Update chatbot.py

--- PythonProject/chatbot.py
+++ PythonProject/chatbot.py
@@ -2,17 +2,25 @@ import random
 import json
 import pickle
 import tkinter as tk
+from tkinter import messagebox
 import nltk
 from nltk.stem import WordNetLemmatizer
 from tensorflow.python.keras.models import load_model
  
 import numpy as np
+
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open("intents.json").read())
  
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 model = load_model('chatbot_model.h5')
+
+# Display terms and conditions messagebox
+response = messagebox.askquestion("DocBot Terms & Conditions", "The DocBot medical chatbot app is provided for informational purposes only and is not intended to be a substitute for professional medical advice, diagnosis, or treatment. The information provided by the DocBot medical chatbot app should not be used for diagnosing or treating a health problem or disease. Always seek the advice of a qualified healthcare provider with any questions you may have regarding a medical condition. The DocBot medical chatbot app is not designed to be used in emergency situations. If you are experiencing a medical emergency, please call your local emergency services or go to the nearest emergency room. The DocBot medical chatbot app does not guarantee the accuracy, completeness, or usefulness of any information provided. The information provided by the DocBot medical chatbot app is subject to change without notice. The DocBot medical chatbot app is not responsible for any actions taken based on the information provided. The user assumes all risks associated with the use of the DocBot medical chatbot app. By using the DocBot medical chatbot app, the user agrees to indemnify and hold harmless DocBot, its affiliates, and their respective officers, directors, employees, agents, licensors, and suppliers from and against any claims, actions, liabilities, losses, damages, and expenses (including reasonable attorneys fees) arising out of or in connection with the users use of the DocBot medical chatbot app. The DocBot medical chatbot app may collect certain personal information from the user, including but not limited to name, age, and health information. The user acknowledges and agrees that such information may be used to improve the DocBot medical chatbot app and for other purposes as outlined in the privacy policy. DocBot reserves the right to modify these terms and conditions at any time without notice. The users continued use of the DocBot medical chatbot app following such modifications constitutes acceptance of the modified terms and conditions. These terms and conditions shall be governed by and construed in accordance with the laws of the jurisdiction in which the DocBot medical chatbot app is provided, without giving effect to any principles of conflicts of law. If any provision of these terms and conditions is held to be invalid or unenforceable, the remaining provisions shall remain in full force and effect. Do you accept the terms and conditions?")
+
+if response == 'no':
+    exit()  # Close the app if the user does not accept
  
  
 def clean_up_sentence(sentence):
@@ -63,21 +71,19 @@ def get_response(intents_list, intents_json):
             result = random.choice(i['responses'])
             break
     return result
+ 
 
-#create the main window object
 root = tk.Tk()
-root.title("Medical Chatbot")
+root.title("DocBot")
 
-#create the text box where the convesation is going to be
-conversation = tk.Text(root, bg="white", fg="black", width=50, height=30)
+conversation = tk.Text(root, bg="white", fg="black", width=80, height=30)
 conversation.pack()
 
 message = tk.StringVar()
-#Create the text box where the user inputs their sentence
+
 message_entry = tk.Entry(root, textvariable=message)
 message_entry.pack()
 
-#the function ran by the send button
 def send_message():
     user_message = message.get()
     ints = predict_class(user_message)
@@ -86,13 +92,13 @@ def send_message():
     conversation.insert("end", "Chatbot: " + res + "\n")
     message.set("")
 
-#creating the button its self
 send_button = tk.Button(root, text="Send", command=send_message)
 send_button.pack()
 
-#initiate the main loop
 root.mainloop()
 
-if name == 'main':
+if __name__ == '__main__':
     print("Bot is Running")
     send_message()
+        
+ 
